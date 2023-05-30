9cd4ad770650c319ec2dfa77f1ad69f3630c82b4
Noman5237 <anonyman637@gmail.com>
2023-05-30 16:00:22 +0600

    added model generation and fixed to lower op

--- backend/src/main/java/com/eastnetic/todoapp/todo/repository/specifications/TodoSpecifications.java
+++ backend/src/main/java/com/eastnetic/todoapp/todo/repository/specifications/TodoSpecifications.java
@@ -2,6 +2,7 @@ package com.eastnetic.todoapp.todo.repository.specifications;
 
 import com.eastnetic.todoapp.common.util.DateTimeUtils;
 import com.eastnetic.todoapp.todo.domain.entity.Todo;
+import com.eastnetic.todoapp.todo.domain.entity.Todo_;
 import com.eastnetic.todoapp.todo.domain.request.TodoFilterRequest;
 import com.eastnetic.todoapp.todo.enums.Status;
 import com.eastnetic.todoapp.user.exception.UserNotFoundException;
@@ -22,22 +23,22 @@ public interface TodoSpecifications {
 			
 			if (filterTodoRequest.getName() != null) {
 				predicates = cb.and(predicates,
-				                    cb.like(root.get("name"), wildcardsAndLower(filterTodoRequest.getName())));
+				                    cb.like(cb.lower(root.get(Todo_.NAME)), wildcardsAndLower(filterTodoRequest.getName())));
 			}
 			if (filterTodoRequest.getStatus() != null) {
 				predicates = cb.and(predicates,
-				                    cb.equal(root.get("status"), Status.fromName(filterTodoRequest.getStatus())));
+				                    cb.equal(root.get(Todo_.STATUS), Status.fromName(filterTodoRequest.getStatus())));
 			}
 			if (filterTodoRequest.getDueDate() != null) {
 				var greaterThanPredicate = cb.greaterThanOrEqualTo(root.get("dueDate"),
 				                                                   filterTodoRequest.getDueDate());
-				var lessThanPredicate = cb.lessThan(root.get("dueDate"),
+				var lessThanPredicate = cb.lessThan(root.get(Todo_.DUE_DATE),
 				                                    DateTimeUtils.addDay(filterTodoRequest.getDueDate(), 1));
 				
 				predicates = cb.and(predicates, greaterThanPredicate, lessThanPredicate);
 			}
 			if (filterTodoRequest.getIsImportant() != null) {
-				predicates = cb.and(predicates, cb.equal(root.get("isImportant"), filterTodoRequest.getIsImportant()));
+				predicates = cb.and(predicates, cb.equal(root.get(Todo_.IS_IMPORTANT), filterTodoRequest.getIsImportant()));
 			}
 			
 			return predicates;
